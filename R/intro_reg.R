9181bb48cb15a974faaf286383d87f53b4e657b6
keurcien <keurcien.luu@gmail.com>
2017-07-21 16:33:00 +0200

    Added functions for evaluation

--- R/intro_reg.R
+++ R/intro_reg.R
@@ -1,29 +1,5 @@
 #' Sample introgression regions
 #'
-#' \code{sample_intro_reg} returns a list of introgressed alleles.
-#'
-#' @param nSNP an integer.
-#' @param n.reg an integer.
-#' @param intro.size an integer.
-#' 
-#' @return 
-#'
-#' @export
-#'
-sample_intro_reg = function(nSNP, n.reg = 5, intro.size){
-  ### Introgression region
-  intro.reg <- NULL
-  idx <- sample(1:nSNP, size = n.reg)
-  gt.df <- data.frame(beg = pmax(1, idx - intro.size), 
-                      end = pmin(nSNP, idx + intro.size))
-  for (k in 1:n.reg){
-    intro.reg <- c(intro.reg, (gt.df$beg[k]):(gt.df$end[k]))
-  }
-  return(list(gt.df = gt.df, intro.reg = intro.reg))
-}
-
-#' Sample introgression regions
-#'
 #' \code{sample_introgression_regions} returns a list of introgressed alleles.
 #'
 #' @param nSNP an integer.
@@ -35,7 +11,7 @@ sample_intro_reg = function(nSNP, n.reg = 5, intro.size){
 #' @export
 #'
 sample_introgression_regions = function(nSNP, n.windows, n.regions){
-  idx <- sample(1:n.windows, size = n.regions)
+  idx <- sort(unique(sample(1:n.windows, size = n.regions)))
   window.size <- nSNP / n.windows
   gt.df <- data.frame(start = (idx - 1) * window.size + 1 , 
                       end = idx * window.size)
--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,44 @@ number.to.string = function(n){
   figures.only <- tokens[[1]][tokens[[1]] != "."]
   return(paste(figures.only, collapse = ""))
 }
+
+#' Utils
+#'
+#' \code{stat.by.window} split a vector of statistics into a certain number
+#' windows.
+#'
+#' @param stat a numeric vector.
+#' @param n.windows an integer specifying the number of windows.
+
+#' 
+#' @return 
+#'
+#' @export
+#'
+stat.by.window = function(stat, 
+                          n.windows, 
+                          window.size){
+  sbw.max <- vector(mode = "numeric", n.windows)
+  sbw.mean <- vector(mode = "numeric", n.windows)
+  subset.min <- (1:n.windows - 1) * window.size + 1
+  subset.max <- (1:n.windows) * window.size
+  sbw.max <- sapply(1:n.windows, FUN = function(h){max(stat[subset.min[h]:subset.max[h]], na.rm = TRUE)})
+  sbw.mean <- sapply(1:n.windows, FUN = function(h){mean(stat[subset.min[h]:subset.max[h]], na.rm = TRUE)})
+  return(list(stat.max = sbw.max, stat.mean = sbw.mean))
+}
+
+#' Utils
+#'
+#' \code{top} 
+#'
+#' @param idx a numeric vector.
+#' @param gt a numeric vector.
+#' @param n an integer.
+#' 
+#' @return 
+#'
+#' @export
+#'
+top = function(idx, gt, n){
+  sum(idx[1:n] %in% gt)
+}
